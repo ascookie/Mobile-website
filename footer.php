816050f9deec7a7481562067b8d1f3b1368140ec
Yuan <yuan@avitalweb.com>
2015-09-16 15:01:52 -0700

    2nd init commit

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div class="mofooter">
+	<span class="bold">The Veneers Dental Office</span><br/> 
+	911 Hampshire Road, Suite 37, Westlake Village, CA 91361
+</div>
--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-index.php
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Veneers Westlake Village</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="style.css">
+		<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+	</head>
+	<body>
+		<div data-role="page">
+			<div data-role="header">
+				<?php include "header.php"; ?>
+			</div>
+			<div role="main" class="ui-content">
+				
+			</div>
+			<div data-role="footer">
+				<?php include "footer.php" ?>
+			</div>
+		</div>
+	</body>
+</html>
