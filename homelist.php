1b2363fbec643ca475e01b1a0cca81031ba72002
Yuan <yuan@avitalweb.com>
2015-09-24 15:27:59 -0700

    added most home text

---
+++ homelist.php
@@ -0,0 +1,9 @@
+<ul data-role="listview">
+	<li><a href="http://m.dentist4veneers.com">Homepage</a></li>
+	<li><a href="#">Meet the Doctors</a></li>
+	<li><a href="#">Smile Analysis</a></li>
+	<li><a href="#">Smile Gallery</a></li>
+	<li><a href="#">Special Offers</a></li>
+	<li><a href="#">Contact Us</a></li>
+</ul>
+
--- index.php
+++ index.php
@@ -21,9 +21,39 @@
 				<a data-theme="b" href="#" class="ui-btn ui-shadow callBtn"><i class="fa fa-phone fa-lg"></i> <i>Call Us Now</i></a>
 
 			</div>
-			<div role="main" class="ui-content">
-				
+			<div class="homelist">
+				<?php include "homelist.php" ?>
 			</div>
+			<div class="callAction center">
+				<a data-theme="b" href="#" class="ui-btn ui-shadow callBtn"><i class="fa fa-phone fa-lg"></i> <i>Call Us Now</i></a>
+
+			</div>
+			<div role="main" class="ui-content mainstyle">
+				<h2 style="text-align: center;">All That Is Good Begins With a SMILE</h2>
+				<p>Dental veneers are often contact-lens thin, porcelain restorations that are as strong as natural enamel and give us the ability to correct imperfections such as chips, gaps, stains, and teeth misalignment. We also utilize porcelain veneers to reshape, lengthen or shorten teeth and create stunning smile makeovers that rejuvenate a smile.</p>
+
+				<p>At our office, we use our years of experience and mastery of esthetics along with our Master Ceramist to create a customized smile for you that looks absolutely radiant and yet feels completely natural.</p>
+
+				<p>Our fantastic team will be there each step of the way to make sure you are comfortable and make you feel at home.</p>
+
+				<h2 style="text-align: center;">VENEERS</h2>
+				<p>
+					If you are looking to correct a major or even minor imperfection in your smile, porcelain veneers can be a great option. Certain things we eat and drink, as well as dental injuries and the natural aging process can all have a negative impact on our teeth and smile. Porcelain dental veneers can correct both aesthetic and functional conditions such as:
+				</p>
+
+				<ul>					
+				    <li>Cracks</li>
+				    <li>Chips</li>
+				    <li>Stains and discolorations</li>
+				    <li>Teeth misalignment</li>
+				    <li>Gaps</li>
+				    <li>Uneven smiles</li>
+				    <li>Worn down teeth</li>
+				    <li>Short-looking teeth</li>
+				    <li>Gummy smiles</li>
+				</ul>
+			</div>
+
 			<div data-role="footer">
 				<?php include "footer.php" ?>
 			</div>
